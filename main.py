7e71afb4ae4ad6f5e682c113203738bf13937106
PWRTwlightTri <grayson.farrell15@outlook.com>
2026-05-09 13:44:22 -0400

    Update main.py
    name change

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    basic.show_string("Grayson")
+    basic.show_string("Twlight")
     music.set_volume(255)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
